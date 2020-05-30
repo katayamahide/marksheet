6829def665920ca9332ec5e2e3be1e26cdf67985
katayamahide <64414172+katayamahide@users.noreply.github.com>
2020-05-30 23:07:21 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,126 @@
+'use strict';
+// const nQuestion = document.getElementById('nQuestion');
+// nQuestion.addEventListener('change', () => {
+//     let numQ = nQuestion.value;
+//     console.log(numQ);
+// });
+// const nChoice = document.getElementById('nChoice');
+// nChoice.addEventListener('change', () => {
+//     let numC = nChoice.value;
+//     console.log(numC);
+// })
+
+// let type1 = document.getElementById("setting");
+// type1.addEventListener('change', () => {
+//     let type = type1.typeC.value;
+// });
+
+// let word = document.getElementById("setting");
+// word.addEventListener('change', () => {
+//     console.log(word.wordC.value);
+// });
+
+const btn = document.getElementById('create');
+btn.addEventListener('click', () => {
+
+    //問題数=numQを取得
+    const nQuestion = document.getElementById('nQuestion');
+    let numQ = nQuestion.value;
+    console.log(numQ);
+
+    //選択肢数=numCを取得
+    const nChoice = document.getElementById('nChoice');
+    let numC = nChoice.value;
+
+    //選択肢の種類=typeを取得
+    const type1 = document.getElementById("setting");
+    let type = type1.typeC.value;
+    console.log(type);
+
+    //選択肢の文字=wordを取得
+    const word1 = document.getElementById("setting");
+    let word = word1.wordC.value;
+
+    const alphabet = new Array('?', 'a', 'b', 'c', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z');
+
+
+    for (let i = 1; i <= numQ; i++) {
+        const main = document.getElementById('target');
+        console.log(main);
+        const div = document.createElement('div');
+        div.classList.add('answer');
+        let id = "q" + i;
+        div.setAttribute('id', id);
+        div.innerHTML = "問" + i;
+        main.appendChild(div);
+
+        if (type === 'single') {
+            for (let i = 1; i <= numC; i++) {
+                const radio = document.createElement('input');
+                let ic = "a" + i;
+                radio.setAttribute('type', 'radio');
+                radio.setAttribute('id', id + ic);
+                radio.setAttribute('name', id);
+                radio.setAttribute('value', i);
+                div.appendChild(radio);
+                let label = document.createElement('label');
+                label.setAttribute('for', id + ic);
+                if (word === "number") {
+                    label.textContent = i;
+                } else if (word === "word") {
+                    label.textContent = alphabet[i];
+                } else {
+                    label.textContent = i;
+                }
+                div.appendChild(label);
+            }
+
+        } else if (type === 'multi') {
+            for (let i = 1; i <= numC; i++) {
+                const checkbox = document.createElement('input');
+                let ic = "a" + i;
+                checkbox.setAttribute('type', 'checkbox');
+                checkbox.setAttribute('id', id + ic);
+                checkbox.setAttribute('name', id);
+                checkbox.setAttribute('value', i);
+                div.appendChild(checkbox);
+                let label = document.createElement('label');
+                label.setAttribute('for', id + ic);
+                if (word === "number") {
+                    label.textContent = i;
+                } else if (word === "word") {
+                    label.textContent = alphabet[i];
+                } else {
+                    label.textContent = i;
+                }
+                div.appendChild(label);
+            }
+        } else {
+            for (let i = 1; i <= numC; i++) {
+                const checkbox = document.createElement('input');
+                let ic = "a" + i;
+                checkbox.setAttribute('type', 'checkbox');
+                checkbox.setAttribute('id', id + ic);
+                checkbox.setAttribute('name', id);
+                checkbox.setAttribute('value', i);
+                div.appendChild(checkbox);
+                let label = document.createElement('label');
+                label.setAttribute('for', id + ic);
+                label.textContent = i;
+                div.appendChild(label);
+            }
+        }
+    }
+});
+
+
+// let radio = document.createElement('input'); radio.setAttribute('type', 'radio'); radio.setAttribute('id', 88); radio.setAttribute('name', id); radio.setAttribute('value', 88);
+
+// let label = document.createElement('label');
+// label.setAttribute('for', 88);
+// label.textContent = 88;
+
+
+// div.appendChild(radio);
+
+// // document.form.appendChild(div);
