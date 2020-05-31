59a98745844cc6a868f6544e1c5554bea36a8d09
username <username@example.com>
2020-05-31 13:42:01 +0900

    margin etc.

--- script.js
+++ script.js
@@ -1,11 +1,44 @@
 'use strict';
+
+//ラベル(id="question")取得
+const question = document.getElementById('question');
+//セレクト(設問の数) 作成
+const selectQ = document.createElement('select');
+selectQ.setAttribute('name', 'nQuestion');
+selectQ.setAttribute('id', 'nQuestion');
+question.appendChild(selectQ);
+
+//オプション作成
+for (let i = 1; i <= 100; i++) {
+    const option = document.createElement('option');
+    option.innerText = i;
+    option.setAttribute('value', i);
+    selectQ.appendChild(option);
+}
+
+//ラベル(id="choice")取得
+const choice = document.getElementById('choice');
+//セレクト(選択肢の数) 作成
+const selectC = document.createElement('select');
+selectC.setAttribute('name', 'nChoice');
+selectC.setAttribute('id', 'nChoice');
+choice.appendChild(selectC);
+
+//オプション作成
+for (let i = 1; i <= 100; i++) {
+    const option = document.createElement('option');
+    option.innerText = i;
+    option.setAttribute('value', i);
+    selectC.appendChild(option);
+}
+
+
 const btn = document.getElementById('create');
 btn.addEventListener('click', () => {
 
     //問題数=numQを取得
     const nQuestion = document.getElementById('nQuestion');
     let numQ = nQuestion.value;
-    console.log(numQ);
 
     //選択肢数=numCを取得
     const nChoice = document.getElementById('nChoice');
@@ -14,7 +47,6 @@ btn.addEventListener('click', () => {
     //選択肢の種類=typeを取得
     const type1 = document.getElementById("setting");
     let type = type1.typeC.value;
-    console.log(type);
 
     //選択肢の文字=wordを取得
     const word1 = document.getElementById("setting");
@@ -25,14 +57,13 @@ btn.addEventListener('click', () => {
 
     for (let i = 1; i <= numQ; i++) {
         const main = document.getElementById('target');
-        console.log(main);
         const div = document.createElement('div');
         div.classList.add('answer');
         let id = "q" + i;
         div.setAttribute('id', id);
         div.innerHTML = "問" + i + " ";
         main.appendChild(div);
-
+        // おっさん
         if (type === 'single') {
             for (let i = 1; i <= numC; i++) {
                 const radio = document.createElement('input');
@@ -41,17 +72,17 @@ btn.addEventListener('click', () => {
                 radio.setAttribute('id', id + ic);
                 radio.setAttribute('name', id);
                 radio.setAttribute('value', i);
-                div.appendChild(radio);
                 let label = document.createElement('label');
                 label.setAttribute('for', id + ic);
                 if (word === "number") {
-                    label.textContent = i + " ";
+                    label.textContent = i + "  ";
                 } else if (word === "word") {
-                    label.textContent = alphabet[i] + " ";
+                    label.textContent = alphabet[i] + "  ";
                 } else {
-                    label.textContent = i + " ";
+                    label.textContent = i + "  ";
                 }
                 div.appendChild(label);
+                label.insertBefore(radio, label.firstChild);
             }
 
         } else if (type === 'multi') {
@@ -62,7 +93,6 @@ btn.addEventListener('click', () => {
                 checkbox.setAttribute('id', id + ic);
                 checkbox.setAttribute('name', id);
                 checkbox.setAttribute('value', i);
-                div.appendChild(checkbox);
                 let label = document.createElement('label');
                 label.setAttribute('for', id + ic);
                 if (word === "number") {
@@ -73,6 +103,7 @@ btn.addEventListener('click', () => {
                     label.textContent = i + " ";
                 }
                 div.appendChild(label);
+                label.insertBefore(checkbox, label.firstChild);
             }
         } else {
             for (let i = 1; i <= numC; i++) {
@@ -82,11 +113,11 @@ btn.addEventListener('click', () => {
                 checkbox.setAttribute('id', id + ic);
                 checkbox.setAttribute('name', id);
                 checkbox.setAttribute('value', i);
-                div.appendChild(checkbox);
                 let label = document.createElement('label');
                 label.setAttribute('for', id + ic);
                 label.textContent = i + " ";
                 div.appendChild(label);
+                label.insertBefore(checkbox, label.firstChild);
             }
         }
     }
